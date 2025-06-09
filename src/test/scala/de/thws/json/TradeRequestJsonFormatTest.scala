fdaadea8d7ea3b3abca88c7bb18435d84f52bcc9
dani162 <34603476+dani162@users.noreply.github.com>
2025-06-09 13:07:39 +0200

    fix leas test

--- src/test/scala/de/thws/json/TradeRequestJsonFormatTest.scala
+++ src/test/scala/de/thws/json/TradeRequestJsonFormatTest.scala
@@ -1,32 +1,32 @@
-//package de.thws.json
-//
-//import de.thws.Boot.TradeRequestJsonFormat
-//import de.thws.domain.{TradeRequest, UserName}
-//import org.scalatest.flatspec.AnyFlatSpec
-//import org.scalatest.matchers.should.Matchers
-//import spray.json.JsonParser
-//
-//class TradeRequestJsonFormatTest extends AnyFlatSpec with Matchers {
-//  
-//  val buyRequestJsonFormat: TradeRequestJsonFormat = new TradeRequestJsonFormat()
-//  
-//  it should "parse a json to BuyRequest" in {
-//    
-//    val name = "Max Mustermann"
-//    val quantity = 5
-//    
-//    val json = JsonParser(
-//      s"""
-//         |{
-//         |  "name": "$name",
-//         |  "quantity": $quantity
-//         |}
-//         |""".stripMargin
-//    )
-//    
-//    val expectedResult = TradeRequest(UserName(name), quantity)
-//    
-//    expectedResult shouldEqual buyRequestJsonFormat.read(json)
-//  }
-//
-//}
+package de.thws.json
+
+import de.thws.domain.{TradeRequest, UserName, WaffleTransactionType}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import spray.json.JsonParser
+
+class TradeRequestJsonFormatTest extends AnyFlatSpec with Matchers {
+
+  val buyRequestJsonFormat: TradeRequestJsonFormat = new TradeRequestJsonFormat()
+
+  it should "parse a json to BuyRequest" in {
+
+    val name = "Max Mustermann"
+    val quantity = 5
+
+    val json = JsonParser(
+      s"""
+         |{
+         |  "${TradeRequestJsonFormat.userName}": "$name",
+         |  "${TradeRequestJsonFormat.quantity}": $quantity,
+         |  "${TradeRequestJsonFormat.transactionType}": "${WaffleTransactionType.Buy.value}"
+         |}
+         |""".stripMargin
+    )
+
+    val expectedResult = TradeRequest(UserName(name), quantity, WaffleTransactionType.Buy)
+
+    expectedResult shouldEqual buyRequestJsonFormat.read(json)
+  }
+
+}
