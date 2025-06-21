eaf3334d9def375ad7454412f7b4633f15b4e036
dani162 <34603476+dani162@users.noreply.github.com>
2025-06-21 14:48:43 +0200

    fix

--- src/it/scala/utils/Utils.scala
+++ src/it/scala/utils/Utils.scala
@@ -3,9 +3,11 @@ package utils
 import io.gatling.core.Predef.*
 import io.gatling.http.Predef.*
 import io.gatling.http.protocol.HttpProtocolBuilder
+import scala.util.Properties
 
 object Utils {
+  val baseUrl: String = Properties.envOrElse("WAFFLE_API_URL", "http://localhost:8080")
   val baseHttpProtocol: HttpProtocolBuilder = http
-    .baseUrl("http://localhost:8080")
+    .baseUrl(baseUrl)
     .acceptHeader("application/json")
 }
