34892f1430093e40a9b393b4e4791a9afaeaa970
Lea Hauser <lea.hauser@study.thws.de>
2025-06-21 17:25:12 +0200

    service

--- src/main/scala/de/thws/service/WafflePriceUpdateService.scala
+++ src/main/scala/de/thws/service/WafflePriceUpdateService.scala
@@ -4,6 +4,7 @@ package service
 import de.thws.domain.{Price, WafflePrice}
 
 import java.util.concurrent.atomic.AtomicReference
+import scala.concurrent.duration.DurationInt
 import scala.util.Random
 
 class WafflePriceUpdateService(
@@ -11,24 +12,32 @@ class WafflePriceUpdateService(
                                 val initial: Double = 2.5
                               ) {
 
-  private val priceRef = new AtomicReference(WafflePrice(Price(this.initial)))
+  private val currentPriceReference = new AtomicReference(WafflePrice(Price(this.initial)))
+  private val ticker = new java.util.Timer()
 
-  private val scheduler = new Runnable {
-    def run(): Unit = {
-      val old = priceRef.get()
-      val change = Random.between(-0.2, 0.2)
-      val newPrice = (old.price.value + change).max(0.5)
-      println(s"New price: $newPrice")
-      val newWafflePrice = WafflePrice(Price(BigDecimal(newPrice).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble))
-      wafflePriceService.add(newWafflePrice)
-      priceRef.set(newWafflePrice)
+  def currentPrice: WafflePrice = currentPriceReference.get()
+
+  private def init(): Unit = {
+
+    val scheduler = new Runnable {
+      def run(): Unit = updatePrice()
     }
+
+    ticker.scheduleAtFixedRate(new java.util.TimerTask {
+      def run(): Unit = scheduler.run()
+    }, 0, 30.seconds.toMillis)
   }
 
-  private val ticker = new java.util.Timer()
-  ticker.scheduleAtFixedRate(new java.util.TimerTask {
-    def run(): Unit = scheduler.run()
-  }, 0, 30000) // 30 Sekunden
+  private def updatePrice(): Unit = {
+    val old = currentPriceReference.get()
+    val change = Random.between(-0.2, 0.2)
+    val newPrice = ((old.price.value + change).max(0.5) * 100).round / 100
+    val newWafflePrice = WafflePrice(Price(newPrice))
+    
+    println(s"New Price: $newWafflePrice")
+    wafflePriceService.add(newWafflePrice)
+    currentPriceReference.set(newWafflePrice)
+  }
 
-  def currentPrice: WafflePrice = priceRef.get()
+  init()
 }
